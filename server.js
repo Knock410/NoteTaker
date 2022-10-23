62ac69af5fa58c8763aed95b41f0cff211166b39
Knock <enoch10evelyn@gmail.com>
2022-10-22 17:42:37 -0700

    added api routes

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 
+const fs = require('fs');
+
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const notes = require('./db/db');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+  
+  res.json(notes);
+});
 
+app.post('/api/notes', (req, res) => {
+  const note = req.body;
+  notes.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(notes)
+  );
+  res.json(notes);
+});
 
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
