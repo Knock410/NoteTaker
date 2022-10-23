94af52e223f5437440bbd9235b579ce5281ae704
Knock <enoch10evelyn@gmail.com>
2022-10-22 18:09:21 -0700

    Edited routes and added comments

--- server.js
+++ server.js
@@ -10,15 +10,17 @@ const app = express();
 
 const notes = require('./db/db');
 
+//Used to set up middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+//Routes to  return data 
 app.get('/api/notes', (req, res) => {
   
   res.json(notes);
 });
-
+//Routes to update  data 
 app.post('/api/notes', (req, res) => {
   const note = req.body;
   notes.push(note);
@@ -28,7 +30,7 @@ app.post('/api/notes', (req, res) => {
   );
   res.json(notes);
 });
-
+//These are routes to return HTML
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   });
@@ -36,7 +38,7 @@ app.post('/api/notes', (req, res) => {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
-
+//app.listen needs to be at the end of the server js to give acess to a port for get and post 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
